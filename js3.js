6150061b4f523f7a4a3876a7c40df2e222920df6
Iván Briones <ivan.brioneslzr@uanl.edu.mx>
2022-10-12 15:33:18 -0500

    Prueba

---
+++ js3.js
@@ -0,0 +1,8 @@
+let hora_toma = prompt ("Hora inicial de toma (0-23):");
+let frecuencia = prompt ("Frecuencia (1-12):");
+
+let tomas = 24/ frecuencia;
+
+console.log("Horario")
+
+let Horario = hora_toma 
