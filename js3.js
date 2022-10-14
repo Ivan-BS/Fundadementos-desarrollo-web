295d4afb98e43860e6dc8a04145d5127b034fe02
Iván Briones <ivan.brioneslzr@uanl.edu.mx>
2022-10-14 14:27:23 -0500

    Corrección js

--- js3.js
+++ js3.js
@@ -1,9 +1,33 @@
-let hora_toma = prompt ("Hora inicial de toma (0-23):");
+/* let hora_toma = prompt ("Hora inicial de toma (0-23):");
 let frecuencia = prompt ("Frecuencia (1-12):");
 
-let tomas = 24/ frecuencia;
+let tomas = 24 / frecuencia;
 
-console.log("Horario")
+console.log("Horario");
 
-let Horario = hora_toma 
+let horario = hora_toma;
 
+for (let i = 1;i <= tomas; i++) {
+    console.log(`Toma: ` + i + " | " + (horario) + ":00 hrs|");
+    horario = horario + frecuencia;
+    if (horario >= 24) {
+        horario = horario - 24;
+    };
+}; */
+
+let hora_toma = 5;
+let frecuencia = 6;
+
+let tomas = 24 / frecuencia;
+
+console.log("Horarios");
+
+let horario = hora_toma;
+
+for (let i = 1;i <= tomas; i++) {
+    console.log(`Toma: ` + i + " | " + (horario) + ":00 hrs|");
+    horario = horario + frecuencia;
+    if (horario >= 24) {
+        horario = horario - 24;
+    };
+};
