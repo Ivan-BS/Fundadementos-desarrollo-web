f6195da2d585d0baa20ac132eb6ab46c4b3e6c2a
Iván Briones <ivan.brioneslzr@uanl.edu.mx>
2022-10-17 10:41:25 -0500

    Archivos

--- js/js4.js
+++ js/js4.js
@@ -9,4 +9,28 @@ function divisores(numero){
     return total;
 }
 
+const divisoresV2 = function(numero){
+    let total = 0;
+    
+    for(let i=1; i < numero; i ++){
+        if(numero % 1 == 0){
+            total++;
+        }
+    }
+    return total;
+}
+
+const divisoresV3 = numero =>{
+    let total = 0;
+    
+    for(let i=1; i < numero; i ++){
+        if(numero % 1 == 0){
+            total++;
+        }
+    }
+    return total;
+}
+
 console.log("Total de  divisores", divisores(32));
+console.log("Total de  divisores", divisoresV2(32));
+console.log("Total de  divisores", divisoresV3(32));
