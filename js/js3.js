bd8871f2d69e81aa55cc535b3449a5cefdd33005
Iván Briones <ivan.brioneslzr@uanl.edu.mx>
2022-10-27 18:49:46 -0500

    link js3

--- js/js3.js
+++ js/js3.js
@@ -31,3 +31,25 @@ for (let i = 1;i <= tomas; i++) {
         horario = horario - 24;
     };
 };
+/* Programa Numeros Perfectos*/
+
+let cant_numeros = prompt("¿Cuantos numeros perfectos quieres ver?");
+let encontrados = 0;
+
+let numero = 0;
+
+document.write(`<h2>${cant_numeros} Numero Perfectos</h2>`);
+while (cant_numeros != encontrados){
+    numero = numero + 1;
+    let suma = 0;
+
+    for (let i = 1; i < numero; i++){
+        if(numero % i == 0 ){
+            suma = suma + i;
+        };
+    };
+    if (suma == numero) {
+        document.write(numero + "<br>");
+        encontrados = encontrados + 1;
+    };
+}
