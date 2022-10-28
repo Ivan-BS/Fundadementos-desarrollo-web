4b9e7958156dad9121e8f8745a39e4c3f7f30c37
Iván Briones <ivan.brioneslzr@uanl.edu.mx>
2022-10-27 19:15:00 -0500

    link js4

--- js/js4.js
+++ js/js4.js
@@ -34,3 +34,28 @@ const divisoresV3 = numero =>{
 console.log("Total de  divisores", divisores(32));
 console.log("Total de  divisores", divisoresV2(32));
 console.log("Total de  divisores", divisoresV3(32));
+
+
+function calcular() {
+    let numero = parseInt(document.getElementById("numero").value);    
+    let resultado = document.getElementById("resul");
+    resultado.value = divisores(numero);
+}
+
+
+function cambiarimg() {
+    let opcion = parseInt(document.getElementById("opcion").value);   
+    let base = document.getElementById("imagen"); 
+    switch (opcion) {
+        case 1:
+            base.src = "https://cdn.britannica.com/15/189715-050-4310222B/Dubai-United-Arab-Emirates-Burj-Khalifa-top.jpg";
+            break;
+        case 2:
+            base.src = "https://lonelyplanetes.cdnstatics2.com/sites/default/files/styles/max_1300x1300/public/fotos/tailandia_krabi_gettyrf_465966551_copyright_anek_getty_images.jpg?itok=DG0Ry5B5";
+            break;
+        case 3:
+            base.src = "https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg";
+            break;
+    }
+    
+}
